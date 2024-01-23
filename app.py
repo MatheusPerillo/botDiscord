12bc32a45ff2a0a0519e47f30dcf5ac70fbc8699
Lucas Canedo <saleslucas12@gmail.com>
2024-01-23 00:53:57 -0300

    Conexão do bot ao discord

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
-nomes = 'lucas e perillo'
-print(nomes)
+import discord
+import os
+from dotenv import load_dotenv
+from typing import Final    
 
-print("Star project")
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith("$henrique quitou"):
+        await message.channel.send("Henrique é um grande merda!")
+
+load_dotenv()
+TOKEN: Final[str] = os.getenv("TOKEN")
+
+if TOKEN is None:
+    print("A variável de ambiente TOKEN não está definida corretamente.")
+else:
+    client.run(TOKEN)
